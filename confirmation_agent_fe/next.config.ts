1192bbf46b6006d314ea9941a7f2d5dbd6378af1
Thylink <al.carmona.al@outlook.com>
2026-01-29 14:52:53 -0600

    Cambios en compose y demas rutas

--- confirmation_agent_fe/next.config.ts
+++ confirmation_agent_fe/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/SchedulerAgent/CallsStatusLogger',
+  // basePath: '/SchedulerAgent/CallsStatusLogger',
   trailingSlash: true, // Crucial: coincidir con la '/' del location de Nginx
   // assetPrefix: '/SchedulerAgent/CallStatusLogger', // Prueba comentando esto si el error persiste
 };
