7742996f8797afe0076e3cbb3204637a9e0551a9
Thylink <al.carmona.al@outlook.com>
2026-02-05 14:14:23 -0600

    Ajustes minimos

--- confirmation_agent_fe/src/app/api/session/route.ts
+++ confirmation_agent_fe/src/app/api/session/route.ts
@@ -8,10 +8,10 @@ const JWT_SECRET = new TextEncoder().encode(
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { accessKey } = body;
-    console.log("Intento de autenticación con clave:", accessKey, "encontrada en env:", process.env.ACCESS_KEY);
+    const { access_key } = body;
+    console.log("Intento de autenticación con clave:", access_key, "encontrada en env:", process.env.ACCESS_KEY);
     
-    if (accessKey === process.env.ACCESS_KEY) {
+    if (access_key === process.env.ACCESS_KEY) {
       const jwt = await new SignJWT({ role: "admin" }) 
         .setProtectedHeader({ alg: "HS256" })
         .setIssuedAt()
