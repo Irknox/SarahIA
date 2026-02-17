9b17596ad811361836aa6481fb062ab6d1e52f2f
Thylink <al.carmona.al@outlook.com>
2026-02-16 18:07:55 -0600

    Se implemente webhook para que el agente pueda avisar voice-recording

--- confirmation_agent_be/agent_api/api.py
+++ confirmation_agent_be/agent_api/api.py
@@ -8,10 +8,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import get_call_context
 from typing import Dict, Any, Optional
 from utils import get_call_context
-
+import redis
 
 load_dotenv()
 Token_API=os.environ.get('NEXT_PUBLIC_AUTH_TOKEN')
+REDIS_URL = os.getenv("REDIS_URL", "redis://:password@127.0.0.1:6380/0")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
+
 
 app = FastAPI(title="SarahAI API")
 
@@ -47,8 +51,7 @@ async def elevenlabs_pre_call_webhook(
         }
     } 
 
-
-@app.post("/webhooks/voice_recording_webhook")
+@app.post("/webhooks/voice-recording")
 async def notify_voice_recording(
         request: Request,    
         auth_token: Optional[str] = Header(None, alias="auth-token")
@@ -56,28 +59,22 @@ async def notify_voice_recording(
     if auth_token != Token_API:
         print(f"Intento de acceso no autorizado con token: {auth_token}")
         raise HTTPException(status_code=401, detail="No autorizado: Token inválido")
+    
     payload = await request.json()
-    print(f"📲 Webhook pre-llamada recibido: {payload}")
-    caller_id = payload.get("caller_id", "")
-    call_id=payload.get("conversation_id", "")
-    call_context= get_call_context(caller_id, call_id)
-    username=call_context.get("username")
-    email=call_context.get("email")
-    type=call_context.get("type")
-    indications=call_context.get("indications")
-    print(f"Contexto para la llamada del usuario {username}: {call_context}")
+    call_id = payload.get("system__conversation_id")
+    
+    if not call_id:
+        raise HTTPException(status_code=400, detail="Falta conversation_id")
+
+    status_key = f"call_status:{call_id}"
+    if not redis_client.exists(status_key):
+        return {"status": "error", "message": "ID de llamada no encontrado en Redis"}
+
+    redis_client.set(status_key, "FAILED", ex=86400)
+    
+    print(f"[Webhook] Buzón detectado en ID {call_id}. Forzando reintento vía Celery Beat.")
+
     return {
-        "type": "conversation_initiation_client_data",
-        "dynamic_variables": {
-            "user_name": username,
-            "type": type,
-            "indications": indications,
-        },
-        "conversation_config_override": {
-            "agent": {
-                "first_message": f"Hola {username}, Soy Sarah de Eurofirms!"
-            }
-        }
-    } 
-
-
+        "status": "success", 
+        "message": "Voice recording notification received, retry triggered"
+    }
