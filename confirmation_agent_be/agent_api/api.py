4bccc065205fa6edbe334908f22d65aef29ec29e
Thylink <al.carmona.al@outlook.com>
2026-02-24 11:09:03 -0600

    Debbugin basic

--- confirmation_agent_be/agent_api/api.py
+++ confirmation_agent_be/agent_api/api.py
@@ -77,9 +77,9 @@ request: Request,
         "type": "conversation_initiation_client_data",
         "conversation_config_override": {
             "agent": {
-        "prompt": {
-            "prompt": f"Eres Sarah, asistente de la compania farmaceutica farmacorp, asistes a los usuarios.",
-        },
+        #"prompt": {
+        #    "prompt": f"Eres Sarah, asistente de la compania farmaceutica farmacorp, asistes a los usuarios.",
+        #},
         "first_message": f"Hola Alejandro, soy Sarah",
     },
             
